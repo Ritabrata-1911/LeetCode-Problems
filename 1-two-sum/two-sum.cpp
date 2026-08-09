bdea979c0a20a72b9fa3db68b220b35f0a225f94
Ritabrata-1911 <ritabrataroychowdhury@zohomail.in>
2026-08-09 20:08:08 +0530

    Time: 2 ms (72.14%) | Memory: 15.1 MB (9.49%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mpp;
+        int i,dif=0;
+        for(i=0;i<nums.size();i++)
+        {
+            dif=target-nums[i];
+            if(mpp.find(dif)!=mpp.end())
+            {
+                return {mpp.find(dif)->second,i};
+            }
+                mpp[nums[i]]=i;
+        }
+        return {};
+    }
+};
