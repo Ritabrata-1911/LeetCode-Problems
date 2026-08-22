9a88672600d523f7be52bb1a7cc9573334596456
Ritabrata-1911 <ritabrataroychowdhury@zohomail.in>
2026-08-23 00:11:41 +0530

    Time: 3 ms (43.84%) | Memory: 8.5 MB (92.35%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) 
+    {
+        int copy=x;
+        long long sum=0;
+        while(x>0)
+        {
+            sum=(sum*10)+(x%10);
+            x/=10;
+        }
+        return (copy>=0 && copy==sum);
+    }
+};
